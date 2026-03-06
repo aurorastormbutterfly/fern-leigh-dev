6f436c207dd0bfcadce8820af1a01e3061dc94a1
aurorastormbutterfly <aurorastormbutterfly@gmail.com>
2026-03-06 13:17:37 +0000

    add unoptimised to image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,12 @@ export default function RootLayout({
                 width={255}
                 height={75}
                 sizes="(max-width: 768px) 255px,
-                      (max-width: 1200px) 383px" 
+                      (max-width: 1200px) 383px"
                 style={{
-          width: '100%',
-          height: 'auto',
-        }}
+                  width: "100%",
+                  height: "auto",
+                }}
+                unoptimized
               />
             </Link>
           </div>
