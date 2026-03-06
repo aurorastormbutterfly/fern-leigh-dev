8d9dc4ef6b058ecfbc8b1a4f286df1707697182f
aurorastormbutterfly <aurorastormbutterfly@gmail.com>
2026-03-06 12:54:40 +0000

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {      // <--- ADD THIS SECTION
+    turbo: false,      // <--- AND THIS LINE
+  }
 };
 
 export default nextConfig;
