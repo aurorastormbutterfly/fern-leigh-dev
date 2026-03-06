66f735286bcc3ad900db5c5335a695f647e6baec
aurorastormbutterfly <aurorastormbutterfly@gmail.com>
2026-03-06 13:06:42 +0000

    remove experimental from next config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  experimental: {      // <--- ADD THIS SECTION
-    turbo: false,      // <--- AND THIS LINE
-  }
-};
+
+  };
 
 export default nextConfig;
