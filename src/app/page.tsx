27d406de71e60c29e0d03f6c892ae0e4923cb10e
aurorastormbutterfly <aurorastormbutterfly@gmail.com>
2026-03-09 10:16:47 +0000

    fix typo and add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import styles from './page.module.scss';
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <section className={styles.hero}>
         <h1>Welcome to my Portfolio</h1>
-        <p>This sit is currently being updated - please check back soon for new content and features!</p>
+        <p>
+          This site is currently being updated - please check back soon for new
+          content and features!
+        </p>
       </section>
     </div>
   );
