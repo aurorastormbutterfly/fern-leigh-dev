113cb652b2a252a54004bfbd893a3adddefc8490
aurorastormbutterfly <aurorastormbutterfly@gmail.com>
2026-03-09 10:25:37 +0000

    add head tag with favicon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import ThemeToggle from "@/components/ThemeToggle/ThemeToggle";
 import Navbar from "@/components/Navbar/Navbar";
 
 export const metadata: Metadata = {
-  title: "Fern Leigh | Portfolio",
+  title: "Fern Leigh Dev | Portfolio",
   description: "Senior Software Engineer Portfolio",
 };
 
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="dark">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body>
         <header className={styles.header}>
           <div className={styles.brand}>
