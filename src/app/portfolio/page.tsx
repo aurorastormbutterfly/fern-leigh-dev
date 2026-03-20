09faa1247518bcd9e9342cbb0b6378f3a758ced9
aurorastormbutterfly <aurorastormbutterfly@gmail.com>
2026-03-20 10:13:57 +0000

    add note to portfolio page and update styling across site

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./portfolio.module.scss";
 import ProjectCard from "@/components/ProjectCard/ProjectCard";
 import { db } from "@/lib/firebase/firebaseAdmin";
+import Link from "next/link";
 
 interface ProjectData {
   title: string;
@@ -22,7 +23,35 @@ export default async function Portfolio() {
 
   return (
     <section className={styles.gallery}>
-      <h3>Projects</h3>
+      <div className={styles.prose}>
+        <p>
+          Much of my professional work is covered by NDAs, which limits what I
+          can share directly. However, I&apos;m currently building an
+          engineering lab to showcase my skills on a smaller scale. This lab
+          will feature a variety of learning projects and code samples from
+          courses I&apos;ve designed.
+        </p>
+        <p>
+          I&apos;m in the process of consolidating projects from various GitLab
+          and GitHub accounts that I&apos;ve used in different roles and
+          locations, while also carefully checking licensing and IP agreements.
+          New projects will be added regularly.
+        </p>
+        <p>
+          For a centralised and up-to-date view of my work, please visit my new
+          professional GitHub account, which will be my sole account going
+          forward:{" "}
+          <Link
+            href="https://github.com/aurorastormbutterfly"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/aurorastormbutterfly
+          </Link>
+          .
+        </p>
+      </div>
+      <h1>Projects</h1>
       <div className={styles.grid}>
         {projectsData.map((project) => (
           <ProjectCard
